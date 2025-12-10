f818fdd2e1955ca3d598a56eb42114234a27a4fb
HP <sanandvin@gmail.com>
2025-12-10 10:46:19 +0000

    Updated ConfigReader.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,4 +1,15 @@
 package base;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
+   @BeforeMethod
+    public static void Login() {
+
+    }
+
+
+    @AfterMethod tearDown() {
+
+    }
 }
--- src/main/java/config/ConfigReader.java
+++ src/main/java/config/ConfigReader.java
@@ -1,4 +1,27 @@
 package config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+    private static Properties prop;
+
+public static void loadConfig() {
+  prop = new Properties();
+    try (FileInputStream fis = new FileInputStream("src/test/resources/config.properties")) {
+        prop.load(fis);
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+
+
+}
+
+public static String get(String key) {
+    if (prop==null) {
+        loadConfig(); // load if not already loaded
+    }
+   return prop.getProperty(key);
+}
 }
