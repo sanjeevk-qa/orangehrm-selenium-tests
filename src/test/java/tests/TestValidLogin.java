8c5aa730c3e2d8a5d8688129c42f29fcad099261
HP <sanandvin@gmail.com>
2026-01-12 11:27:34 +0000

    Implimented Extendreport and testing.xml

--- src/test/java/tests/TestValidLogin.java
+++ src/test/java/tests/TestValidLogin.java
@@ -11,7 +11,7 @@ public class TestValidLogin extends BaseTest {
     @Test
     public  void testValidLogin()  {
         LoginPage login = new LoginPage(driver);
-        login.loginAs("Admi","admin123");
+        login.loginAs("Admin","admin123");
         System.out.println("Successfull login");
 
         DashboardPage dashboard= new DashboardPage(driver);
